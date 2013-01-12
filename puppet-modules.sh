79d5051f9ff5a3a95ce345f394cc3c263fc92dca
Robert Starmer <starmer@cisco.com>
2013-01-12 00:27:29 +0000

    update puppet-modules.sh to include pointers to other repo modules

--- puppet-modules.sh
+++ puppet-modules.sh
@@ -6,13 +6,20 @@ FILE_LIST=modules.list
 RELEASE=folsom
 REPO=https://github.com/CiscoSystems/
 PUPPET_PATH=/etc/puppet/
+PUPPET_MODULES=/etc/puppet/modules
 
 while IFS= read -r module
 do
         # display $line or do somthing with $line
 	# echo "$module"
-	git clone -b $RELEASE "$REPO"puppet-"$module".git "$PUPPET_PATH"modules/$module
+	git clone -b $RELEASE "$REPO"puppet-"$module".git "$PUPPET_MODULES"/$module
 done <"$FILE_LIST"
 
 # TODO: This module does not follow the naming "puppet-module" convention
-git clone -b $RELEASE "$REPO"puppetlabs-lvm.git "$PUPPET_PATH"modules/lvm
+git clone -b $RELEASE "$REPO"puppetlabs-lvm.git "$PUPPET_MODULES"/lvm
+
+# current intereim step, adding these packages rather than including them in modules.list
+git clone https://github.com/iawells/puppet-openstack -b folsom "$PUPPET_MODULES"/openstack
+git clone https://github.com/iawells/openstack-quantum-puppet -b ian-refactor "$PUPPET_MODULES"/quantum
+git clone https://github.com/iawells/puppet-vswitch-1 "$PUPPET_MODULES"/vswitch
+git clone https://github.com/robertstarmer/puppet-sshroot "$PUPPET_MODULES"/sshroot

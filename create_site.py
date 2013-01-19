5fa7dbaa91528ec62cbb9c4cf57e482d8ae9880a
Robert Starmer <rstarmer@gmail.com>
2013-01-19 00:49:55 -0800

    Update site.pp.example and site.pp.template
    Remove unused variables (Quantum deployment)
    Improve site.yaml.example
    Clean up create_site.py with hardcoded /etc/puppet/manifests path
    Update Readme

--- create_site.py
+++ create_site.py
@@ -3,9 +3,9 @@ import yaml
 from passlib.apps import custom_app_context as pwd_context
 from jinja2 import Template, Environment, FileSystemLoader
 
-test = yaml.load(open("site.yaml"))
-site = open("site.pp","w")
-env = Environment(loader=FileSystemLoader('./'))
+test = yaml.load(open("/etc/puppet/manifests/site.yaml"))
+site = open("/etc/pupet/manifests/site.pp","w")
+env = Environment(loader=FileSystemLoader('/etc/puppet/manifests/'))
 if test['default']['password']:
   test['default']['password'] = pwd_context.encrypt(test['default']['password'])
 template = env.get_template('site.pp.template')

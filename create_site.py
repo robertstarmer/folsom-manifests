2cc7f910fb70227a931da09dfe4eceda66bb3a02
Robert Starmer <starmer@cisco.com>
2013-01-19 01:14:31 -0800

    Fix pupet -> puppet typo

--- create_site.py
+++ create_site.py
@@ -4,7 +4,7 @@ from passlib.apps import custom_app_context as pwd_context
 from jinja2 import Template, Environment, FileSystemLoader
 
 test = yaml.load(open("/etc/puppet/manifests/site.yaml"))
-site = open("/etc/pupet/manifests/site.pp","w")
+site = open("/etc/puppet/manifests/site.pp","w")
 env = Environment(loader=FileSystemLoader('/etc/puppet/manifests/'))
 if test['default']['password']:
   test['default']['password'] = pwd_context.encrypt(test['default']['password'])
